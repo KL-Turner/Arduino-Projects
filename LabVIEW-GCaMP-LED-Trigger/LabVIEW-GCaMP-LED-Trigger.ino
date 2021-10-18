10ddef3945b458d73f18da9ca4e9e4ce01bee558
Kevin Turner <klt8@psu.edu>
2021-10-18 19:05:42 -0400

    .

--- LabVIEW-GCaMP-LED-Trigger/LabVIEW-GCaMP-LED-Trigger.ino
+++ LabVIEW-GCaMP-LED-Trigger/LabVIEW-GCaMP-LED-Trigger.ino
@@ -9,14 +9,14 @@
 //            temporary save-delay, a BK Precision 4017a Frequency Generator will immediately pick-up the 60 Hz flashing so that there is no
 //            noticeable gap in stimuli to the animal.
 //
-//   Last Updated: 07/03/19
+//   Last Updated: 10/15/21
 //____________________________________________________________________________________________________________________________________________
 
 // Initializes pins to be represented by a variable (int <variable name> = <pin #>).
 int IOScam = 9;               // IOS camera output pin
 int blueLEDs = 3;             // blue LEDs output pin (4 T-Cube LED drivers attached to a BNC breakout board).
-int greenLEDs = 12;           // green LEDs output pin (4 T-Cube LED drivers attached to a BNC breakout board).
-int inputSource = 5;          // Waveform Generator - Square wave with adjustable frequency. Serves as the Input for times between trials.
+int greenLEDs = 5;            // green LEDs output pin (4 T-Cube LED drivers attached to a BNC breakout board).
+int inputSource = 11;         // Waveform Generator - Square wave with adjustable frequency. Serves as the Input for times between trials.
 int labviewTrialState = 7;    // Input from LabVIEW to indicate when data acquisition is running
 int monitor = 13;             // For debugging/testing
 
@@ -31,12 +31,12 @@ int prevTrialState = 0;
 // The setup routine runs only once when the Arduino is turned on, sketch is uploaded to the board, or is reset.
 void setup()
 {
-  pinMode(IOScam, OUTPUT);                // IOS camera (pin 9) as an Output
-  pinMode(blueLEDs, OUTPUT);              // blue LEDs (pin 3) is an Output.
-  pinMode(greenLEDs, OUTPUT);             // green LEDs (pin 12) is an Output
-  pinMode(labviewTrialState, INPUT);      // LabVIEW trial state (pin 1) is an Input.
-  pinMode(inputSource, INPUT);            // inputSource (pin 0) is an Input via the Waveform Generator.
-  pinMode(monitor, OUTPUT);
+  pinMode(IOScam,OUTPUT);                // IOS camera (pin 9) as an Output
+  pinMode(blueLEDs,OUTPUT);              // blue LEDs (pin 3) is an Output.
+  pinMode(greenLEDs,OUTPUT);             // green LEDs (pin 12) is an Output
+  pinMode(labviewTrialState,INPUT);      // LabVIEW trial state (pin 1) is an Input.
+  pinMode(inputSource,INPUT);            // inputSource (pin 0) is an Input via the Waveform Generator.
+  pinMode(monitor,OUTPUT);
 }
 
 // The loop routine runs continuously, i.e. functions are not simultaneous, they run in order.
@@ -46,7 +46,7 @@ void loop()
   digitalWrite(monitor, trialState);            // For testing purposes
   if (trialState == HIGH)
   {
-    inputSource = 11;
+    inputSource = 10;
     if (prevTrialState == 0)
     {
       prevBlueState = 1;                        // Ensure that the first flash of every new trial is from green LEDs
@@ -56,7 +56,7 @@ void loop()
   }
   else
   {
-    inputSource = 5;
+    inputSource = 11;
     prevTrialState = 0;
   }
   counterState = digitalRead(inputSource);      // Reads state of counter pin and stores it in the 'counterState' variable
@@ -68,13 +68,13 @@ void loop()
       {
         if (trialState == HIGH)                 // Only send pulse to IOScamera if the trial is running
         {
-          digitalWrite(IOScam, HIGH);
+          digitalWrite(IOScam,HIGH);
         }
         else                                    // If trial is not running ensure only lows are sent to the IOS camera
         {
-          digitalWrite(IOScam, LOW);
+          digitalWrite(IOScam,LOW);
         }
-        digitalWrite(blueLEDs, HIGH);           // Sending HIGH signal to blue LEDs regardless of trialState
+        digitalWrite(blueLEDs,HIGH);           // Sending HIGH signal to blue LEDs regardless of trialState
         prevBlueState = 1;                      // Conforms the state variable of blue LEDs to what its state is (blue LEDs is HIGH, so state variable changed to 1)
         prevGreenState = 0;                     // Conforms the state variable of greenL EDs to what its state is (green LEDs is LOW, so state variable changed to 0)
       }
@@ -82,13 +82,13 @@ void loop()
       {
         if (trialState == HIGH)
         {
-          digitalWrite(IOScam, HIGH); 
+          digitalWrite(IOScam,HIGH); 
         }
         else
         {
-          digitalWrite(IOScam, LOW);
+          digitalWrite(IOScam,LOW);
         }
-        digitalWrite(greenLEDs, HIGH); 
+        digitalWrite(greenLEDs,HIGH); 
         prevBlueState = 0;                      // Conforms the state variable of blue LEDs to what its state is (blue LEDs is LOW, so state variable changed to 0)
         prevGreenState = 1;                     // Conforms the state variable of green LEDs to what its state is (blue LEDs is HIGH, so state variable changed to 1)
       }      
@@ -97,14 +97,14 @@ void loop()
     {
       if (trialState == HIGH)
       {
-        digitalWrite(IOScam, LOW);
+        digitalWrite(IOScam,LOW);
       }
       else
       {
-        digitalWrite(IOScam, LOW); 
+        digitalWrite(IOScam,LOW); 
       }
-      digitalWrite(blueLEDs, LOW); 
-      digitalWrite(greenLEDs, LOW);  
+      digitalWrite(blueLEDs,LOW); 
+      digitalWrite(greenLEDs,LOW);  
     }
     prevCounterState = counterState;             // Stores the current state of the counter to the last counter state variable
   }
